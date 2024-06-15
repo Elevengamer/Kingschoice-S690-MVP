ca1bcc59cac7ac84da5c8864637df8ab44484249
Elevengamer <99888182+Elevengamer@users.noreply.github.com>
2024-06-15 21:29:02 +0200

    prepare data structure

---
+++ DesktopApp/KINGS_Choice_MVP/KINGS_Choice_MVP/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KINGS_Choice_MVP
+{
+    class Repository
+    {
+    }
+
+
+    class Player
+    {
+        private String name;
+        private String uid;
+        private String fine;
+        private DateTime lastMVP;
+    }
+    class MVP
+    {
+        private Player player;
+
+    }
+}
